f02c75174de1f09650727e84c405a8ae05911ac3
nmouahbi <83113523+nmouahbi@users.noreply.github.com>
2022-08-26 13:28:24 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './nav/about/about.component';
+import { ContactUsComponent } from './nav/contact-us/contact-us.component';
+import { FleetComponent } from './nav/fleet/fleet.component';
+import { HomeComponent } from './nav/home/home.component';
+import { RatesComponent } from './nav/rates/rates.component';
+import { ReservationComponent } from './nav/reservation/reservation.component';
+import { ServicesComponent } from './nav/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  { path: 'home',component: HomeComponent },
+  { path: 'about',component: AboutComponent },
+  { path: 'fleet', component: FleetComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'reservation', component: ReservationComponent },
+  { path: 'rates', component: RatesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './nav/home/home.component';
+import { AboutComponent } from './nav/about/about.component';
+import { ServicesComponent } from './nav/services/services.component';
+import { FleetComponent } from './nav/fleet/fleet.component';
+import { ContactUsComponent } from './nav/contact-us/contact-us.component';
+import { RatesComponent } from './nav/rates/rates.component';
+import { ReservationComponent } from './nav/reservation/reservation.component';
+import { HeaderComponent } from './main/header/header.component';
+import { FooterComponent } from './main/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    FleetComponent,
+    ContactUsComponent,
+    RatesComponent,
+    ReservationComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
